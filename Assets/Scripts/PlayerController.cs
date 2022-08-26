04184521875638a73eb342df349ceb65acad7048
Shivansh Ahuja <kartikaye02@gmail.com>
2022-08-26 21:17:00 +0530

    Added Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player Movement Handlers")]
+    public float playerForwardMoveSpeed = 5f;
+    public float playerSidewardsMoveSpeed = 3f;
+    public float playerIncreaseSpeed = 1f;
+    public float playerJumpSpeed = 5f;
+
+    private float targetSpeed = 35f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayerForward();
+
+        ChangePlayerSides();
+
+        PlayerJump();
+    }
+
+    void MovePlayerForward()
+    {
+        //TURN IT ON LATER!
+      /* if (playerMoveSpeed < targetSpeed)
+        {
+            playerMoveSpeed += playerIncreaseSpeed * Time.deltaTime;
+        }*/ 
+
+        transform.Translate(Vector3.forward * playerForwardMoveSpeed * Time.deltaTime);
+    }
+
+    void ChangePlayerSides()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal") * playerSidewardsMoveSpeed * Time.deltaTime;
+        transform.Translate(Vector3.right * horizontalInput);
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector3(0f, playerJumpSpeed, 0f), ForceMode.Impulse);
+        }
+    }
+}
