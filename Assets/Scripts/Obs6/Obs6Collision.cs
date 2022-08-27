4fdd74b393ddc461816a4ca2018e7ff8d3ad964a
Shivansh Ahuja <kartikaye02@gmail.com>
2022-08-27 16:47:05 +0530

    Fixed Bugs

---
+++ Assets/Scripts/Obs6/Obs6Collision.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obs6Collision : MonoBehaviour
+{
+    public GameObject roller;
+    private bool collided = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (collided)
+        {
+            roller.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 20f, 50f));
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            roller.AddComponent<Rigidbody>();
+            collided = true;
+        }      
+    }
+}
